aa4115f3c6857cf31f3430d9302655dab9e8a792
revako <128324698+revako@users.noreply.github.com>
2023-03-21 11:57:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function move() {
         }
     }
 
-    if (ballY >= canvas.height - paddleHeight - ballRadius) {
+        if (ballY >= canvas.height - paddleHeight - ballRadius) {
         if (ballX >= bottomPaddleX - ballRadius && ballX <= bottomPaddleX + paddleWidth + ballRadius) {
             ballSpeedY = -ballSpeedY;
             increaseBallSpeed();
@@ -70,74 +70,42 @@ function increaseBallSpeed() {
     ballSpeedY *= 1.0125;
 }
 
-function generateQRCode(url) {
-    const qr = qrcode(0, 'L');
-    qr.addData(url);
-    qr.make();
-    return qr.createDataURL(4);
-}
-
 function stopGame() {
     gameInProgress = false;
-
-    // Generate the QR code
-    const qrCodeUrl = generateQRCode('https://revako.github.io/');
-    const qrCodeImage = document.createElement('img');
-    qrCodeImage.src = qrCodeUrl;
-    qrCodeImage.style.display = 'block';
-    qrCodeImage.style.position = 'absolute';
-    qrCodeImage.style.left = (canvas.width / 2 - 50) + 'px'; // Adjust horizontal position
-    qrCodeImage.style.top = (canvas.height / 2 + 60) + 'px'; // Adjust vertical position
-    qrCodeImage.id = 'qrCodeImage'; // Add an ID to the QR code image for easier removal
-    document.body.appendChild(qrCodeImage);
 }
 
 function resetGame() {
-    const qrCodeToRemove = document.getElementById('qrCodeImage');
-    if (qrCodeToRemove) {
-        qrCodeToRemove.remove();
-    }
-
-    // Rest of the resetGame function remains the same
-    const startingPoints = [
-        { x: canvas.width / 4, y: canvas.height / 4 },
-        { x: canvas.width / 2, y: canvas.height / 4 },
-        { x: (3 * canvas.width) / 4, y: canvas.height / 4 },
-        { x: canvas.width / 4, y: canvas.height / 2 },
-        { x: canvas.width / 2, y: canvas.height / 2 },
-        { x: (3 * canvas.width) / 4, y: canvas.height / 2 },
-        { x: canvas.width / 4, y: (3 * canvas.height) / 4 },
-        { x: canvas.width / 2, y: (3 * canvas.height) / 4 },
-        { x: (3 * canvas.width) / 4, y: (3 * canvas.height) / 4 },
-    ];
-
-    const randomStartingPoint = startingPoints[Math.floor(Math.random() * startingPoints.length)];
-
-    ballX = randomStartingPoint.x;
-    ballY = randomStartingPoint.y;
+    ballX = canvas.width / 2;
+    ballY = canvas.height / 2;
     ballSpeedX = 1;
     ballSpeedY = 2.5;
     hitCounter = 0;
     gameInProgress = true;
-    shareButton.style.display = "none";
 }
 
-// Add this event listener outside of the stopGame function
-canvas.addEventListener('click', () => {
-    if (!gameInProgress) {
-        resetGame();
-    }
-});
-
-// Rest of the code
-canvas.addEventListener("mousemove", (event) => {
-    const mouseX = event.clientX - canvas.getBoundingClientRect().left;
+function handlePaddleMovement(clientX) {
+    const mouseX = clientX - canvas.getBoundingClientRect().left;
     const newPaddleX = mouseX - paddleWidth / 2;
 
-    // Make sure the paddle stays within the canvas
     if (newPaddleX >= 0 && newPaddleX <= canvas.width - paddleWidth) {
         topPaddleX = newPaddleX;
         bottomPaddleX = newPaddleX;
+    }
+}
+
+canvas.addEventListener("mousemove", (event) => {
+    handlePaddleMovement(event.clientX);
+});
+
+canvas.addEventListener("touchmove", (event) => {
+    event.preventDefault();
+    const touch = event.touches[0];
+    handlePaddleMovement(touch.clientX);
+});
+
+canvas.addEventListener("click", () => {
+    if (!gameInProgress) {
+        resetGame();
     }
 });
 
